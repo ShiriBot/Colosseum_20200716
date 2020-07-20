14915d53bcf7f8f4c030ad1bf07a794c8f9d37a3
채정실 <cowjdtlf@gmail.com>
2020-07-20 19:45:01 +0900

    비밀번호 변경에 따른 로그 작성

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/LoginActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/LoginActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.colosseum_20200716
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
@@ -19,6 +20,12 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun SetupEvents() {
+
+        signUpBtn.setOnClickListener {
+
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
+        }
 
         loginBtn.setOnClickListener {
 //            입력한 아이디와 비번을 받아서
