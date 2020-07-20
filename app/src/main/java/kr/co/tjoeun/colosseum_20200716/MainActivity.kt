41b7f8ed0137885ae0f3b0a635883b7192e5d017
채정실 <cowjdtlf@gmail.com>
2020-07-20 22:21:12 +0900

    서버에서 토픽목록 불러오기... GET 활용

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
@@ -2,9 +2,13 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.colosseum_20200716.datas.Topic
+import kr.co.tjoeun.colosseum_20200716.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
+    val mTopicList = ArrayList<Topic>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,17 @@ class MainActivity : BaseActivity() {
     }
 
     override fun SetValues() {
+        getTopicListFromServer()
+    }
+
+    fun getTopicListFromServer(){
+
+        ServerUtil.getRequestMainInfo(mContext, object: ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+
+            }
+        })
+
 
     }
 
--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/utils/ServerUtil.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.colosseum_20200716.utils
 import android.content.Context
 import android.util.Log
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.json.JSONObject
 import java.io.IOException
 
@@ -14,16 +15,76 @@ class ServerUtil {
         fun onResponse(json: JSONObject)
     }
 
-//    JAVA의 static에 대응되는 개졈
+//    JAVA의 static에 대응되는 개념
 
     companion object {
 
         //        호스트 주소를 저장해두는 변수
         private val BASE_URL = "http://15.165.177.142"
 
+//        메인화면에서 쓸 토론주제 목록을 가져오는 기능
+
+        fun getRequestMainInfo(context: Context, handler: JsonResponseHandler?) {
+
+            val client = OkHttpClient()
+
+//            GET / DELETE : query에 파라미터 첨부
+//            query => 주소(url)에 직접 어떤 데이터가 담기는지 기록.
+//            주소를 적을때 => 파라미터 첨부도 같이 진행해야 함.
+
+            val urlBuilder = "${BASE_URL}/v2/main_info".toHttpUrlOrNull()!!.newBuilder()
+
+//            urlBuilder에 필요한 파라미터를 첨부하면 됨
+            urlBuilder.addEncodedQueryParameter("dvice_token", "TEST기기토큰")
+            urlBuilder.addEncodedQueryParameter("os", "Android")
+
+//            모든 데이터가 담겼으면 주소를 완성해서 String으로 저장.
+            val urlString = urlBuilder.build().toString()
+
+//            실제 요청 정보를 request 변수에 종합
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .header("X-Http-Token", ContextUtil.getLogtinUserToken(context))
+                .build()
+
+//            완성된 요청 정보를 실제로 호출 => 응답처리
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+//                    서버 연결 자체에 실패한 경우
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                    연결은 성공해서, 서버가 응답을 내려줬을 때 실행됨.
+//                    실제로 서버가 내려준 응답 내용을 변수로 저장.
+                    val bodyStr = response.body?.string()
+
+//                    응갑 내용으로 Json 객체 생성
+                    val json = JSONObject(bodyStr)
+
+//                    최종적으로 가져온 내용을 로그로 출력
+                    Log.d("서버 응답 내용", json.toString())
+
+//                    Handler 변수에 응답 처리 코드가 들어있다면 실행해주자.
+                    handler?.onResponse(json)
+
+
+                }
+
+            })
+
+        }
+
+
         //        로그인 API를 호출해주는 기능
 //        1. 화면에서 어떤 데이터를 받아와야 하는지? email, pw
-        fun postRequestLogin(context: Context,email:String, pw:String, handler: JsonResponseHandler?) {
+        fun postRequestLogin(
+            context: Context,
+            email: String,
+            pw: String,
+            handler: JsonResponseHandler?
+        ) {
 
 //            서버 통신 담당 변수 (클라이언트 역할 수행 변수)
             val client = OkHttpClient()
@@ -36,7 +97,7 @@ class ServerUtil {
 
             val formData = FormBody.Builder()
                 .add("email", email)
-                .add("password",pw)
+                .add("password", pw)
                 .build()
 
 //            요청 정보를 종합하는 변수 Request 사용
@@ -77,8 +138,14 @@ class ServerUtil {
         }
 
 
-//        회원가입 호출 기능
-        fun putRequestSignUp(context: Context,email:String, pw:String, nickName:String, handler: JsonResponseHandler?) {
+        //        회원가입 호출 기능
+        fun putRequestSignUp(
+            context: Context,
+            email: String,
+            pw: String,
+            nickName: String,
+            handler: JsonResponseHandler?
+        ) {
 
 //            서버 통신 담당 변수 (클라이언트 역할 수행 변수)
             val client = OkHttpClient()
@@ -91,8 +158,8 @@ class ServerUtil {
 
             val formData = FormBody.Builder()
                 .add("email", email)
-                .add("password",pw)
-                .add("nick_name",nickName)
+                .add("password", pw)
+                .add("nick_name", nickName)
                 .build()
 
 //            요청 정보를 종합하는 변수 Request 사용
