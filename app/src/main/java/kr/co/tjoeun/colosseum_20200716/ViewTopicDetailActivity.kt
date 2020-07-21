e0ba84d52a80ce4cd770a13f0c12b0c49027573d
채정실 <cowjdtlf@gmail.com>
2020-07-21 21:13:43 +0900

    상세화면에서 토론 주제 / 이미지 화면에 반영  => FrameLayout으로 겹쳐서 표현

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/ViewTopicDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/ViewTopicDetailActivity.kt
@@ -2,8 +2,20 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_view_topic_detail.*
+import kr.co.tjoeun.colosseum_20200716.datas.Topic
+import kr.co.tjoeun.colosseum_20200716.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewTopicDetailActivity : BaseActivity() {
+
+//    메인화면에서 넘겨준 주제 id 저장
+    var mTopicId = 0 //일단 Int임을 암시
+
+//    서버에서 받아오는 토론 정보를 저장할 멤버변수
+    lateinit var mTopic : Topic
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +28,43 @@ class ViewTopicDetailActivity : BaseActivity() {
 
 
     override fun SetValues() {
+
+//        메인에서 넘겨준 id값을 멤버변수에 저장
+//        만약 0이 저장되었다면 => 오류가 있는 상황으로 간주하자
+        mTopicId = intent.getIntExtra("topicId",0)
+
+        if (mTopicId==0){
+            Toast.makeText(mContext,"주제 상세 id에 문제가 있습니다.",Toast.LENGTH_SHORT).show()
+        }
+
+//        서버에서 토론 주제에 대한 상세 진행 상황 가져오기
+        getTopicDetailFromServer()
+    }
+
+    fun getTopicDetailFromServer(){
+
+        ServerUtil.getRequestTopicDetail(mContext,mTopicId,object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+
+                val data = json.getJSONObject("data")
+
+                val topicObj = data.getJSONObject("topic")
+
+//                서버에서 내려주는 주제 정보를
+//                Topic 객체로 변환해서 멤버변수에 저장
+
+                mTopic = Topic.getTopicFromJson(topicObj)
+
+//                화면에 토론 관련 정보 표시
+
+                runOnUiThread{
+
+                    topicTitleTxt.text = mTopic.title
+                    Glide.with(mContext).load(mTopic.imageUrl).into(topicImg)
+
+                }
+            }
+        })
 
     }
 
