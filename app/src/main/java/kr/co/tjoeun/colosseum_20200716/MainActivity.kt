a44cc58b44532b7631bf1e460f94e093b837ef4d
채정실 <cowjdtlf@gmail.com>
2020-07-21 19:49:09 +0900

    Topic클래스에 json 변환 기능 추가 => 메인 화면에서 JSONArray 파싱 때 사용.

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
@@ -28,13 +28,39 @@ class MainActivity : BaseActivity() {
         ServerUtil.getRequestMainInfo(mContext, object: ServerUtil.JsonResponseHandler{
             override fun onResponse(json: JSONObject) {
 
+                val data = json.getJSONObject("data")
+
+//                topics는 [ ] 임. =>JSONArray로 추출해야함.
+
+                val topics =  data.getJSONArray("topics")
+
+//                topics 내부에는 JSONObject 가 여러개 반복으로 들어있다.
+//                JSON을 들고 있는 배열 => JSONArray
+//                i가 0부터 ~ topics의 갯수 직전. 4개 : (0,1,2,3)
+
+                for (i in 0 until topics.length()){
+
+//                    topics 내부의 데이터를 JSONObject로 추출
+                    val topicObj = topics.getJSONObject(i)
+
+//                    topicObj => Topc 형태의 객체로 변환
+
+                    val topic = Topic.getTopicFromJson(topicObj)
+
+//                    변환된 객체를 목록에 추가
+
+                    mTopicList.add(topic)
+
+                }
+
+
             }
         })
 
 
     }
 
-    override fun SetupEvents() {
+   override fun SetupEvents() {
 
     }
 
