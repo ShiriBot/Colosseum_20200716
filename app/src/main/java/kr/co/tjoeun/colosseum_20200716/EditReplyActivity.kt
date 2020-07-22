d46cdfb8593779f0190199ce8f4370adc862c46a
채정실 <cowjdtlf@gmail.com>
2020-07-22 22:19:54 +0900

    의견 작성 화면에서 토론 주제/ 선택한 진영 보여주기

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/EditReplyActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/EditReplyActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_reply.*
 
 class EditReplyActivity : BaseActivity() {
 
@@ -11,6 +12,9 @@ class EditReplyActivity : BaseActivity() {
     }
 
     override fun SetValues() {
+
+        topicTitleTxt.text = intent.getStringExtra("topicTitle")
+        mySideTitleTxt.text = intent.getStringExtra("selectedSideTitle")
 
     }
 
--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/ViewTopicDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/ViewTopicDetailActivity.kt
@@ -135,7 +135,17 @@ class ViewTopicDetailActivity : BaseActivity() {
         voteToSecondSideBtn.setOnClickListener (voteCode)
 
         postReplyBtn.setOnClickListener {
+
+//            투표를 안한 상태라면, 작성 화면 진입 거부 (투표부터 시키자)
+
+            if (mTopic.mySideId==-1){
+                Toast.makeText(mContext,"투표를 해야만 의견을 작성할 수 있습니당.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             val myIntent = Intent(mContext, EditReplyActivity::class.java)
+            myIntent.putExtra("topicTitle",mTopic.title)
+            myIntent.putExtra("selectedSideTitle",mTopic.mySide!!.title)
             startActivity(myIntent)
         }
 
