b7af653f069e54fee1e1ba2c7e0f82d6f7defc34
채정실 <cowjdtlf@gmail.com>
2020-07-23 22:06:58 +0900

    리스트뷰 내부 버튼에 대한 이벤트 처리  => mContext를 이용해서 화면 시작

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
@@ -1,6 +1,7 @@
 package kr.co.tjoeun.colosseum_20200716.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,14 +9,13 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import kr.co.tjoeun.colosseum_20200716.R
+import kr.co.tjoeun.colosseum_20200716.ViewReplyDetailActivity
 import kr.co.tjoeun.colosseum_20200716.datas.Reply
 import kr.co.tjoeun.colosseum_20200716.utils.TimeUtil
-import okhttp3.internal.format
-import java.text.SimpleDateFormat
 
-class ReplyAdapter (val mcontext: Context , resId : Int, val mList : List<Reply>): ArrayAdapter<Reply> (mcontext, resId, mList) {
+class ReplyAdapter (val mContext: Context, resId : Int, val mList : List<Reply>): ArrayAdapter<Reply> (mContext, resId, mList) {
 
-    val inf = LayoutInflater.from(mcontext)
+    val inf = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
@@ -58,6 +58,18 @@ class ReplyAdapter (val mcontext: Context , resId : Int, val mList : List<Reply>
         likeBtn.text = "좋아요 ${data.likeCount}"
         dislikeBtn.text = "싫어요 ${data.dislikeCount}"
         rereplyBtn.text = "답글 ${data.replyCount}"
+
+//        답글 버튼이 눌리면 => 의견 상세 화면으로 진입
+        rereplyBtn.setOnClickListener {
+
+            val myIntent = Intent(mContext,ViewReplyDetailActivity::class.java)
+//            startActivity 함수는 AppCompatActivity가 내려주는 기능.
+//            Adapter는 액티비티가 아니므로, startActivity 기능을 내려주지 않는다.
+//            mContext 변수가 , 어떤 화면이 리스트뷰를 뿌리는지 들고 있다.
+//            mContext를 이용해서 액티비티를 열어주자.
+            mContext.startActivity(myIntent)
+
+        }
 
         return row
 
